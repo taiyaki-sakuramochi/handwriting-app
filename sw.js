e4f2f98482babf778497b7e7ad25d5fbf6f05a3a
taiyaki-sakuramochi <chirunoromi@gmail.com>
2025-11-20 12:45:08 +0900

    Initial commit of PWA file

---
+++ sw.js
@@ -0,0 +1,79 @@
+const CACHE_NAME = 'handwriting-memo-app-v2'; // Incremented version
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512x512.png'
+];
+
+// Install event: opens the cache and adds the core files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activate event: cleans up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch event: serves from network first, falls back to cache, and caches new requests
+self.addEventListener('fetch', event => {
+  // We only want to cache GET requests.
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          // If not, try to serve from cache as a fallback
+          return caches.match(event.request).then(cacheResponse => {
+            return cacheResponse || response;
+          });
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        // Network request failed, try to get it from the cache.
+        return caches.match(event.request)
+          .then(response => {
+            // Return response from cache or a simple offline page if not in cache.
+            return response;
+          });
+      })
+  );
+});
